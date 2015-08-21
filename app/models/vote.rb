bdc52002442e14ecd68cffa66b229e6943067316
Olexandr Chornodid <chornodid@gmail.com>
2015-08-21 17:47:21 +0300

    Fix bugs in ip address validation and belongs_to

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,9 @@
+require 'pollynesia/patterns'
+
 class Vote < ActiveRecord::Base
-  belongs_to :option
-  belongs_to :user
+  belongs_to :option, required: true
+  belongs_to :user, required: true
 
   validates_format_of :ip_address, with: Pollynesia::Patterns::IP_ADDRESS,
-                                   on: create, allow_nil: true
+                                   on: :create, allow_nil: true
 end
