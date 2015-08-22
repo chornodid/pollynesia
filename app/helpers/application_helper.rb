dc50b64cf7269ff2cf69a6360b711e6dd76e000d
Olexandr Chornodid <chornodid@gmail.com>
2015-08-22 12:12:55 +0300

    Refactor logic to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def bootstrap_alert_class_for_flash_type(type)
+    type == 'notice' ? 'alert-info' : 'alert-danger'
+  end
+
+  def bootstrap_label_class_for_poll_status(status)
+    { 'draft' => 'label-warning',
+      'open' => 'label-success',
+      'closed' => 'label-danger'
+    }[@poll.status.to_s] || 'label-default'
+  end
 end
